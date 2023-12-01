04341e347b19cb316bfc288061b3d8f25883ee0d
Débora Ornellas <debora.barreto.ornellas@lego.com>
2023-12-01 16:41:19 +0000

    Day 1 part 1

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,15 +1,15 @@
-import { day1 } from './day1';
-import { data, testData } from './day1.data';
-import { logAnswer } from '../utils/logging';
+import { day1 } from "./day1";
+import { data, testData } from "./day1.data";
+import { logAnswer } from "../utils/logging";
 
 /* test('Personal test cases', () => {
   expect(day1()).toBe(1);
 }); */
 
-test('Provided test cases', () => {
-  expect(day1(testData as string[])).toBe(1);
+test("Provided test cases", () => {
+  expect(day1(testData as string[])).toBe(142);
 });
 
-test('Returns an answer', () => {
+test("Returns an answer", () => {
   logAnswer(day1(data as string[]));
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,3 +1,17 @@
 export const day1 = (input: string[]) => {
-  return 1;
+  const isDigit = (char: string) => Number.isInteger(Number(char));
+  let sum = 0;
+
+  input.forEach((s) => {
+    let firstDigit = "";
+    let lastDigit = "";
+    for (let i = 0; i < s.length; i++) {
+      if (isDigit(s.charAt(i))) {
+        if (!firstDigit) firstDigit = s.charAt(i);
+        lastDigit = s.charAt(i);
+      }
+    }
+    sum += Number(`${firstDigit}${lastDigit}`);
+  });
+  return sum;
 };
